fb8286357c938a131e790c8cb6c6fa73f6070ef4
Alex Hughes <aphughes@talk21.com>
2018-11-25 16:13:46 +0000

    Cleaned up code

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'users#new'
-  resources :users, only: [:new, :create, :show, :edit, :update] do
-    resources :requirements, only: [:new, :create]
-    resources :user_requirements, only: [:new, :create]
-  end
+  resources :users, only: [:new, :create, :show, :edit, :update]
 end
